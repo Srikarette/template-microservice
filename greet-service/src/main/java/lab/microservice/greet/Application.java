3d6d7b1b1057a46189dbbebd9154e31a06ed88f8
Srikarette <opkungkeke@gmail.com>
2023-08-13 14:18:37 +0700

    Update

--- greet-service/src/main/java/lab/microservice/greet/Application.java
+++ greet-service/src/main/java/lab/microservice/greet/Application.java
@@ -1,14 +1,15 @@
-package lab.microservice.sample;
-
+package lab.microservice.greet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
